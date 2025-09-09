f1fe4c9ab38b723dbb969d1598f61682f4514f9b
Harish0027 <sutarharish143@gmail.com>
2025-09-09 23:12:08 +0530

    redis and rabbitMQ intrgrated

--- services/user/src/utils/GoogleConfig.ts
+++ services/user/src/utils/GoogleConfig.ts
@@ -6,8 +6,11 @@ dotenv.config();
 const GOOGLE_CLIENT_ID = process.env.Google_Client_id;
 const GOOGLE_CLIENT_SECRET = process.env.Google_client_secret;
 
+// Must match exactly what's in Google Cloud Console
+const REDIRECT_URI = "http://localhost:5000/api/auth/google/callback";
+
 export const oauth2client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  "postmessage"
+  REDIRECT_URI
 );
